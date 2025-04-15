607a41f542083e6fe735c2e4954642927fdfbc73
Ddeveloper Emon Hossain <emonhossain4475@gmail.com>
2025-04-15 11:06:59 +0600

    1

---
+++ Cloudberry_Jam.cpp
@@ -0,0 +1,21 @@
+// In the name of Allah
+// Emon Hossain, KYAU
+
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    cout << n * 2 << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
